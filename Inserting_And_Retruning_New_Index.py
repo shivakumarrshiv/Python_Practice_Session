047f0ce6dea88a9482f6249cfd01ec79028460e9
shivakumarrshiv <shivakumarr914837@gmail..com>
2025-03-22 12:33:34 +0530

    Added New Problems

--- Inserting_And_Retruning_New_Index.py
+++ Inserting_And_Retruning_New_Index.py
@@ -1,5 +1,5 @@
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def searchInsert(self, nums: list[int], target: int) -> int:
         nums.append(target)
         nums.sort()
         return nums.index(target)
--- Sorted_String.py
+++ Sorted_String.py
@@ -1,4 +1,4 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(self, nums: list[int]) -> int:
         nums[:] = sorted(set(nums))
         return len(nums)      
